296d70b785b64dcc33017bc86550fbf5ebd2fcf9
Peter Dolak <peter.dolak@7segments.com>
2015-01-28 09:13:43 +0100

    bulk api conformance, refactoring

--- infinario.py
+++ infinario.py
@@ -16,9 +16,9 @@ except NameError:
     basestring = (str, bytes)
 
 
-DEFAULT_TARGET = 'https://api.infinario.com'
+DEFAULT_TARGET = 'https://api.infinario.com/'
 DEFAULT_LOGGER = logging.getLogger(__name__)
-ASYNC_BUFFER_MAX_SIZE = 49  # number of customer updates and events before flushing
+ASYNC_BUFFER_MAX_SIZE = 50  # number of customer updates and events before flushing
 ASYNC_BUFFER_TIMEOUT = 1  # max seconds before buffer is flushed
 
 
@@ -31,11 +31,11 @@ class ServiceUnavailable(Exception):
 
 
 class NullTransport(object):
-    '''
+    """
     NullTransport will make no requests.
 
     It is useful for disabling tracking in the Infinario constructor.
-    '''
+    """
 
     def __init__(self, *_args, **_kwargs):
         pass
@@ -48,11 +48,11 @@ class NullTransport(object):
 
 
 class SynchronousTransport(object):
-    '''
+    """
     SynchronousTransport is a simple synchronous transport using request.Session.
 
     Infinario methods identify, track, update and get_html will block for the whole time of a request.
-    '''
+    """
 
     def __init__(self, target, session=None, logger=None):
         self._logger = logger
@@ -93,13 +93,13 @@ class SynchronousTransport(object):
         self._send(url, message)
 
 
-class WorkerData(object):
+class _WorkerData(object):
     def __init__(self, **kwargs):
         self.__dict__.update(**kwargs)
 
 
 class AsynchronousTransport(object):
-    '''
+    """
     AsynchronousTransport is a buffered asynchronous transport using one lazy-initialized thread and requests.Session.
      This transport requires that the close method is called once the client will no longer be used.
 
@@ -107,12 +107,12 @@ class AsynchronousTransport(object):
      methods identify, track, update, flush and close are non-blocking (consult class Infinario for more information).
     Asynchronous commands will be buffered up to ASYNC_BUFFER_MAX_SIZE of commands and will be flushed at most after
      ASYNC_BUFFER_TIMEOUT seconds.
-    '''
+    """
 
 
     def __init__(self, target, session=None, logger=None):
         # any variables used by more than one thread shall be here
-        self._worker_data = WorkerData(
+        self._worker_data = _WorkerData(
             logger=logger,
             transport=SynchronousTransport(target=target, session=session, logger=logger),
             buffer=[],
@@ -151,7 +151,7 @@ class AsynchronousTransport(object):
                     timeout_in = data.buffer[0]['scheduled'] + ASYNC_BUFFER_TIMEOUT - time.time() if size > 0 else None
                     timeouted = timeout_in is not None and timeout_in < 0
 
-                    if data.flush or size > ASYNC_BUFFER_MAX_SIZE or timeouted:
+                    if (size > 0 and data.flush) or size > ASYNC_BUFFER_MAX_SIZE or timeouted:
                         self._send_bulk()
                         if len(data.buffer) == 0:
                             data.flush = False
@@ -164,11 +164,12 @@ class AsynchronousTransport(object):
 
             def _send_bulk(self):
                 indexes = range(len(data.buffer))
+                message = {'commands': data.buffer[:ASYNC_BUFFER_MAX_SIZE]}
 
                 data.cv.release()
 
                 leftovers, errors = [], []
-                results = data.transport.send_and_receive('/bulk', data.buffer)['results']
+                results = data.transport.send_and_receive('bulk', message)['results']
 
                 data.cv.acquire()
 
@@ -184,7 +185,9 @@ class AsynchronousTransport(object):
                         errors.append((command, status))
 
                 for error in errors:
-                    message = 'Infinario API bulk command failed with status {}'.format(error[1])
+                    message = 'Infinario API bulk command failed with status {}, errors: {}'.format(
+                        error[1], '. '.join(results[i].get('errors', []))
+                    )
                     if data.logger:
                         data.logger.exception(message)
                     else:
@@ -214,43 +217,43 @@ class _InfinarioBase(object):
 
 
 class AuthenticatedInfinario(_InfinarioBase):
-    '''
+    """
     Authenticated Infinario API access for exporting analysis data.
-    '''
+    """
 
     def __init__(self, username, password, target=None):
-        '''
+        """
         :param username: Username for an Infinario account with ExtAPI access
         :param password: Password for the account above
         :param target: Tracking API URL
-        '''
+        """
         super(AuthenticatedInfinario, self).__init__(target)
         session = requests.Session()
         session.auth = (username, password)
         self._transport = SynchronousTransport(target=self._target, session=session, logger=None)
 
     def export_analysis(self, analysis_type, data, token=None):
-        '''
+        """
         Compute and obtain data from an existing Infinario analysis definition
 
         :param analysis_type: funnel/report/retention/segmentation
         :param data: See http://guides.infinario.com/technical-guide/export-api/
         :param token: In case the Infinario account has access to multiple projects, specify the project token
-        '''
+        """
         params = {} if token is None else {'project': token}
         return self._transport.send_and_receive(
-            '/analytics/{}'.format(analysis_type),
+            'analytics/{}'.format(analysis_type),
             data, params
         )
 
 
 class Infinario(_InfinarioBase):
-    '''
+    """
     Infinario API access for tracking events, updating customer data and requesting campaign data.
-    '''
+    """
 
     def __init__(self, token, customer=None, target=None, silent=True, logger=None, transport=SynchronousTransport):
-        '''
+        """
         :param token: Project token to track data into.
         :param customer: Optional identifier of tracked customer (can later be done with method `identify`).
         :param target: Tracking API URL
@@ -258,41 +261,41 @@ class Infinario(_InfinarioBase):
         :param logger: Instance of a logger used with silent == True
         :param transport: One of `NullTransport`, `SynchronousTransport`, `AsynchronousTransport`;
             consult their documentation as well
-        '''
+        """
         super(Infinario, self).__init__(target)
         self._token = token
         self._customer = self._convert_customer_argument(customer)
         self._logger = logger or DEFAULT_LOGGER if silent else None
-        self._transport = transport(target=target, logger=self._logger)
+        self._transport = transport(target=self._target, logger=self._logger)
 
     def identify(self, customer=None, properties=None):
-        '''
+        """
         Identify a customer, optionally update their properties.
         :param customer: Customer identifier
         :param properties: Optional dictionary of properties
-        '''
+        """
         self._customer = self._convert_customer_argument(customer)
         if properties is not None:
             self.update(properties)
 
     def update(self, properties):
-        '''
+        """
         Update the properties of the currently identified customer.
         :param properties: Dictionary of properties
-        '''
-        self._transport.send_and_ignore('/crm/customers', {
+        """
+        self._transport.send_and_ignore('crm/customers', {
             'ids': self._customer,
             'project_id': self._token,
             'properties': properties
         })
 
     def track(self, event_type, properties=None):
-        '''
+        """
         Track an event for the currently identified customer.
         :param event_type: Type of the event to track.
         :param properties: Optional dictionary of properties
-        '''
-        self._transport.send_and_ignore('/crm/events', {
+        """
+        self._transport.send_and_ignore('crm/events', {
             'customer_ids': self._customer,
             'project_id': self._token,
             'type': event_type,
@@ -300,12 +303,12 @@ class Infinario(_InfinarioBase):
         })
 
     def get_html(self, html_campaign_name):
-        '''
+        """
         Get the HTML code to display in case the customer is targeted in a HTML campaign action.
         :param html_campaign_name: Name of the campaign
         :return: HTML code to display
-        '''
-        response = self._transport.send_and_receive('/campaigns/html/get', {
+        """
+        response = self._transport.send_and_receive('campaigns/html/get', {
             'customer_ids': self._customer,
             'project_id': self._token,
             'html_campaign_name': html_campaign_name
@@ -313,17 +316,17 @@ class Infinario(_InfinarioBase):
         return response['data']
 
     def flush(self):
-        '''
+        """
         If using asynchronous buffered transport, this will flush all tracked data to the API.
-        '''
+        """
         getattr(self._transport, 'flush', lambda: None)()
 
     def close(self):
-        '''
+        """
         If using asynchronous buffered transport, this method MUST be called when the client is no longer to be used.
 
         Aside from flushing the buffers, it will also destroy the worker thread.
-        '''
+        """
         getattr(self._transport, 'stop', lambda: None)()
 
     @staticmethod
--- test_infinario_sdk.py
+++ test_infinario_sdk.py
@@ -13,7 +13,7 @@ except ImportError:
 def _fake_post_response(*args, **kwargs):
     mock = MagicMock()
     mock.status_code = 200
-    mock.json = lambda: {'success': True, 'results': [{'status': 'ok'}]*20 + [{'status': 'retry'}]*100}
+    mock.json = lambda: {'success': True, 'results': [{'status': 'ok'}]*20 + [{'status': 'retry'}]*50}
     return mock
 
 
@@ -23,7 +23,7 @@ class TestInfinarioSDK(unittest.TestCase):
     def _pop_post_calls(self, mock):
         calls = []
         for call in [call for call in mock.mock_calls if call[0] == '().post']:
-            match = re.match('^nope(.*)$', call[1][0])
+            match = re.match('^nope/(.*)$', call[1][0])
             self.assertTrue(match)
             self.assertEquals({'Content-type': 'application/json'}, call[2]['headers'])
             calls.append((match.group(1), json.loads(call[2]['data'])))
@@ -35,10 +35,10 @@ class TestInfinarioSDK(unittest.TestCase):
         calls = self._pop_post_calls(mock)
         self.assertEquals(len(expected_leftovers), len(calls))
         for call, expected in zip(calls, expected_leftovers):
-            self.assertEquals('/bulk', call[0])
+            self.assertEquals('bulk', call[0])
             events = 0
-            for command in call[1]:
-                self.assertEquals('/crm/events', command['name'])
+            for command in call[1]['commands']:
+                self.assertEquals('crm/events', command['name'])
                 self.assertEquals(type, command['data']['type'])
                 events += 1
             self.assertEquals(expected, events)
@@ -46,25 +46,25 @@ class TestInfinarioSDK(unittest.TestCase):
     # testing unidentified track, identify, identified track, update and identify at init with simple sync transport
     @patch.object(requests, 'Session')
     def test_synchronous_transport(self, session_mock):
-        infinario = Infinario('t', target='nope', transport=SynchronousTransport)
+        infinario = Infinario('t', target='nope/', transport=SynchronousTransport)
         infinario.track('e1', {'prop1': 'val'})
         infinario.identify(customer='joe', properties={'prop2': 'val'})
         infinario.track('e2')
         infinario.update({'prop3': 'val'})
 
         expected = [
-            ('/crm/events', {'customer_ids': {}, 'project_id': 't', 'properties': {'prop1': 'val'}, 'type': 'e1'}),
-            ('/crm/customers', {'ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {'prop2': 'val'}}),
-            ('/crm/events', {'customer_ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {}, 'type': 'e2'}),
-            ('/crm/customers', {'ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {'prop3': 'val'}})
+            ('crm/events', {'customer_ids': {}, 'project_id': 't', 'properties': {'prop1': 'val'}, 'type': 'e1'}),
+            ('crm/customers', {'ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {'prop2': 'val'}}),
+            ('crm/events', {'customer_ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {}, 'type': 'e2'}),
+            ('crm/customers', {'ids': {'registered': 'joe'}, 'project_id': 't', 'properties': {'prop3': 'val'}})
         ]
         self.assertEquals(expected, self._pop_post_calls(session_mock))
 
-        infinario = Infinario('u', customer='john', target='nope')
+        infinario = Infinario('u', customer='john', target='nope/')
         infinario.track('e1', {'prop1': 'val'})
 
         expected = [
-            ('/crm/events',
+            ('crm/events',
              {'customer_ids': {'registered': 'john'}, 'project_id': 'u', 'properties': {'prop1': 'val'}, 'type': 'e1'})
         ]
         self.assertEquals(expected, self._pop_post_calls(session_mock))
@@ -75,19 +75,19 @@ class TestInfinarioSDK(unittest.TestCase):
         session_mock().post.side_effect = _fake_post_response
 
         # flush
-        infinario = Infinario('t', target='nope', transport=AsynchronousTransport)
+        infinario = Infinario('t', target='nope/', transport=AsynchronousTransport)
         infinario.track('e1')
         infinario.flush()
 
         time.sleep(0.2)
         self._assert_bulk_events(session_mock, [1], 'e1')
 
-        # full buffer, will 2x push 20 through
+        # full buffer, will 2x push 20 through, attempts max 50
         for _ in range(80):
             infinario.track('e2')
 
         time.sleep(0.2)
-        self._assert_bulk_events(session_mock, [80, 60], 'e2')
+        self._assert_bulk_events(session_mock, [50, 50], 'e2')
 
         # timeout, again 2x 20
         time.sleep(1.1)
