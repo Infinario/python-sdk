b4f16106f6e1a298d773631c73057c05caf8f5a6
Martin Sucha <martin.sucha@infinario.com>
2015-11-05 12:36:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='infinario',
+    py_modules=['infinario'],
+    version='1.0.0',
+    install_requires=['requests>=2.2.1'],
+    description='Infinario Python SDK',
+    url='https://github.com/infinario/python-sdk',
+    author='Peter Dolak',
+    author_email='peter.dolak@infinario.com',
+    license='Apache 2.0',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
