362723e927fdf11b8f3602f2e0315f4a6cd87ffe
Peter Dolak <peter.dolak@7segments.com>
2015-12-08 12:57:18 +0100

    Deprecate AsynchronousTransport

--- test_infinario_sdk.py
+++ test_infinario_sdk.py
@@ -6,19 +6,11 @@ else:
     import unittest
 import re
 import requests
-import time
-from infinario import Infinario, SynchronousTransport, AsynchronousTransport
+from infinario import Infinario, SynchronousTransport
 try:
     from mock import MagicMock, patch
 except ImportError:
     from unittest.mock import MagicMock, patch
-
-
-def _fake_post_response(*args, **kwargs):
-    mock = MagicMock()
-    mock.status_code = 200
-    mock.json = lambda: {'success': True, 'results': [{'status': 'ok'}]*20 + [{'status': 'retry'}]*50}
-    return mock
 
 
 class TestInfinarioSDK(unittest.TestCase):
@@ -33,19 +25,6 @@ class TestInfinarioSDK(unittest.TestCase):
             calls.append((match.group(1), json.loads(call[2]['data'])))
         mock.reset_mock()
         return calls
-
-    # helper for assertions about API bulk requests
-    def _assert_bulk_events(self, mock, expected_leftovers, type):
-        calls = self._pop_post_calls(mock)
-        self.assertEquals(len(expected_leftovers), len(calls))
-        for call, expected in zip(calls, expected_leftovers):
-            self.assertEquals('bulk', call[0])
-            events = 0
-            for command in call[1]['commands']:
-                self.assertEquals('crm/events', command['name'])
-                self.assertEquals(type, command['data']['type'])
-                events += 1
-            self.assertEquals(expected, events)
 
     # testing unidentified track, identify, identified track, update and identify at init with simple sync transport
     @patch.object(requests, 'Session')
@@ -76,38 +55,7 @@ class TestInfinarioSDK(unittest.TestCase):
         ]
         self.assertEquals(expected, self._pop_post_calls(session_mock))
 
-    # testing async transport - flush, buffer fill, timeout and close
-    @unittest.skip("This test uses sleep() which means that it fails randomly. Skip it until it is fixed.")
-    @patch.object(requests, 'Session')
-    def test_asynchronous_transport(self, session_mock):
-        session_mock().post.side_effect = _fake_post_response
-
-        # flush
-        infinario = Infinario('t', target='nope/', transport=AsynchronousTransport)
-        try:
-            infinario.track('e1')
-            infinario.flush()
-
-            time.sleep(0.2)
-            self._assert_bulk_events(session_mock, [1], 'e1')
-
-            # full buffer, will 2x push 20 through, attempts max 50
-            for _ in range(80):
-                infinario.track('e2')
-
-            time.sleep(0.2)
-            self._assert_bulk_events(session_mock, [50, 50], 'e2')
-
-            # timeout, again 2x 20
-            time.sleep(1.1)
-            self._assert_bulk_events(session_mock, [40, 20], 'e2')
-        finally:
-            # close
-            infinario.track('e3')
-            infinario.close()
-
-            time.sleep(0.2)
-            self._assert_bulk_events(session_mock, [1], 'e3')
+    # asynchronous transport test removed as it is deprecated
 
 
 class TestConvertTimestampArgument(unittest.TestCase):
